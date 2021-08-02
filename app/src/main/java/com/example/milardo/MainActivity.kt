e7c98e57b6b9278109be964aab16cf6af28dc590
faisal-t <faisalmahromi35@gmail.com>
2021-08-02 17:19:09 +0700

    first commit

---
+++ app/src/main/java/com/example/milardo/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.example.milardo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.webkit.WebChromeClient
+import android.webkit.WebView
+import android.webkit.WebViewClient
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val webView = findViewById<WebView>(R.id.webview)
+
+        webView.loadUrl("https://millardo.online/login")
+        webView.settings.allowFileAccess = true
+        webView.settings.javaScriptEnabled = true
+        webView.settings.allowContentAccess = true
+
+
+        webView.webViewClient = object : WebViewClient() {
+            override fun shouldOverrideUrlLoading(view: WebView?, url: String?): Boolean {
+                url?.let {
+                    view?.loadUrl(it)
+                }
+                return true
+            }
+        }
+    }
+}
